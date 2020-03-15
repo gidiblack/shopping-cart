743cfca5a572e5c2109eb33c45c14102654d33e0
Tha Gidi <you@example.com>
2020-03-15 02:08:30 +0100

    loader fixed

--- store.js
+++ store.js
@@ -6,11 +6,6 @@
 //     },3000 )
 // })()
 
-$(window).load(function() {
-    // Animate loader off screen
-    $(".se-pre-con").fadeOut("slow");;
-});
-
 
 // Event listeners
 var removeCartItemButtons = document.getElementsByClassName("btn-danger");
